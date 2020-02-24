d9567802d5b7905681f5c420bb36b979286fec23
aimee-segment <38016467+aimee-segment@users.noreply.github.com>
2020-02-23 23:10:10 -0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var myTest = require('./testModule'); //makes call to testModule.js file located in same directory
-
-myTest.sayHello();
-myTest.sayGoodbye();
-
-var myCar = require('./carTest');
-
-//var markdown = require('markdown').markdown; //the module handbook tells us we need to use require('markdown').markdown...
-//console.log( markdown.toHTML('A paragraph in **markdown**!')); //prints out <p>A paragraph in <strong>markdown</strong>!</p>
